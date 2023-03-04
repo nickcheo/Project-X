c23e3bd83d0bcf7dfbdf728a5a5383a6a862f232
nickcheo <99509288+nickcheo@users.noreply.github.com>
2023-03-04 15:11:17 -0500

    finally

--- project-x/src/App.js
+++ project-x/src/App.js
@@ -1,46 +1,23 @@
-// import React from 'react';
-// // import logo from './logo.svg';
-// import './App.css';
-// // import Home from "./pages/Home";
-// // import { Router ,Routes, Route } from "react-router-dom";
-// import { Link } from "react-router-dom";
-
-// function App() {
-//   return (
-//     <div className="App">
-            
-//         <p>hi</p>
-//         <ul>
-//           <li>
-//             <Link to="/">Home</Link>
-            
-//           </li>
-//         </ul>
-        
-//       {/* <Router>
-//       <Routes>
-//         <Route exact path="/" component={Home} />
-//       </Routes>
-//       </Router> */}
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-// import Home from './pages/Home';
-// import About from './pages/About';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
 // import Contact from './pages/Contact';
 
 function App() {
   return (
     <div className="App">
       <p>hi</p>
+      
+      <Router>
+        <Link to="/">help me</Link>
+        <br></br>
+        <Link to="/about">help me2</Link>
       <Routes>
-        <Route  path="/" element={Home} />
+        <Route  path="/" element={<Home/>} />
+        <Route  path="/about" element={<About/>} />
       </Routes>
+      </Router>
     </div>
   );
 }
---
+++ project-x/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada tristique nibh, ac feugiat orci vestibulum sed. Morbi nec augue mauris. Donec id lacus eros. Nulla quis maximus lorem. Proin quis nulla nulla. Quisque ultrices velit nec turpis auctor ultricies. Duis gravida iaculis mi, vel viverra nibh ultrices a.</p>
+      <p>Etiam iaculis semper leo nec lacinia. Etiam rhoncus odio eget eros lacinia, vel tincidunt leo volutpat. Fusce ut enim vel purus ultrices malesuada a nec sapien. Fusce ut orci vitae odio fringilla vehicula. Integer vestibulum ligula sed suscipit iaculis. Duis nec enim mauris. Donec dictum auctor tellus, eu rutrum enim laoreet ut.</p>
+    </div>
+  );
+}
+
+export default About;
