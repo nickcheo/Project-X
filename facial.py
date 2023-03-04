4c28a01ddddf64edda86e971703f0bbcb2a13a0d
Vraj Shah <shahvraj26@gmail.com>
2023-03-04 12:30:35 -0500

    test

--- facial.py
+++ facial.py
@@ -1,31 +1,51 @@
 import cv2
+import numpy as np
+import pychrom
 
-# Load the Haar cascade classifier for face detection
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-# Access the webcam
+# Set up video capture
 cap = cv2.VideoCapture(0)
 
+# Create PyChrom object
+pc = pychrom.PyChrom()
+
+# Set ROI for forehead
+x, y, w, h = 200, 100, 200, 100
+
 while True:
-    # Capture frames from the webcam
+    # Read frame from video capture
     ret, frame = cap.read()
 
-    # Convert the frames to grayscale
+    # Flip the frame horizontally for a mirror effect
+    frame = cv2.flip(frame, 1)
+
+    # Convert the frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Apply the Haar cascade classifier to detect faces
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    # Detect face using Haar cascade classifier
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    # Draw a rectangle around the detected faces
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # Draw rectangle around the face
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
 
-    # Display the frames with the detected faces
-    cv2.imshow('Facial detection', frame)
+        # Extract ROI from forehead
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
 
-    # Exit the loop and release resources when the user closes the window or presses a key
+        # Apply PyChrom to estimate heart rate
+        hr = pc.get_heart_rate(roi_color, (x, y, w, h), show_video=False)
+
+        # Display heart rate on frame
+        cv2.putText(frame, f"Heart rate: {hr:.2f} bpm", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+
+    # Exit on 'q' key press
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+# Release video capture and close all windows
 cap.release()
 cv2.destroyAllWindows()
