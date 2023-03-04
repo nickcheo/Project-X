f00fc4b3883168c9a87705f54fd9e2ff63c7e10c
nickcheo <99509288+nickcheo@users.noreply.github.com>
2023-03-04 12:57:35 -0500

    test

--- src/router.js
+++ src/router.js
@@ -6,11 +6,21 @@ import About from './components/About.vue';
 Vue.use(VueRouter);
 
 const routes = [
- 
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  }
 ];
 
 const router = new VueRouter({
   mode: 'history',
+  base: process.env.BASE_URL,
   routes
 });
 
