58c1e63343c51d34cd59c801a4885ad897acd1e4
nickcheo <99509288+nickcheo@users.noreply.github.com>
2023-03-05 04:22:45 -0500

    fuck you

--- src/hbDetect.py
+++ src/hbDetect.py
@@ -2,61 +2,103 @@ import cv2
 from deepface import DeepFace
 import numpy as np
 import matplotlib.pyplot as plt
+import tkinter as tk
+import serial
 
 fearValues = []
 avgFear = []
 
-faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# Define the serial port and baud rate for the ESP32
+ser = serial.Serial('/dev/cu.usbserial-0001', 9600)
 
-cap = cv2.VideoCapture(0)
+def update_slider():
+    # Display the current slider value in the label
+    value_label.config(text=f"Slider value: {slider.get()}")
 
-with open('output.txt', 'w') as f:
-    while True:
-        # Read the next frame
-        ret, frame = cap.read()
+def send_value():
+    # Send the current slider value to the ESP32
+    value = slider.get()
+    ser.write(str(value).encode())
+    # Display a message to indicate that the value was sent
+    status_label.config(text=f"Sent value: {value}")
 
-        frame = cv2.flip(frame, 1)
+# Create the main window
+window = tk.Tk()
 
-        small_frame = cv2.resize(frame, (0, 0), fx=0.2, fy=0.2, interpolation = cv2.INTER_CUBIC)
+# Create a slider and add it to the window
+slider = tk.Scale(window, from_=0, to=255, orient=tk.HORIZONTAL, command=update_slider)
+slider.pack()
 
-        result = DeepFace.analyze(frame, actions = ['emotion'], enforce_detection=False)
-        print(result[0]['emotion']['fear'])
-        fearValues.append(result[0]['emotion']['fear'])
-        avgFearTemp = np.mean(fearValues[-60:])
-        avgFear.append(avgFearTemp)
+# Create a label to display the current slider value
+value_label = tk.Label(window, text="Slider value: 0")
+value_label.pack()
 
-        f.write(str(avgFearTemp)+ '\n')
-        f.flush()
+# Create a "Submit" button
+submit_button = tk.Button(window, text="Submit", command=send_value)
+submit_button.pack()
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = faceCascade.detectMultiScale(gray,1.1,4)
+# Create a label to display the status of the submission
+status_label = tk.Label(window, text="")
+status_label.pack()
 
-        for (x, y, w, h) in faces:
-            cv2.rectangle(small_frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+window.mainloop()
 
-        font = cv2.FONT_HERSHEY_SIMPLEX
+def on_closing():
+    
+    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-        cv2.putText(small_frame,
-                    str(result[0]['dominant_emotion']),
-                    (50, 200),
-                    font, 3,
-                    (0, 0, 255),
-                    2,
-                    cv2.LINE_4)
 
-        cv2.imshow('Video', small_frame)
+    cap = cv2.VideoCapture(0)
 
-        if cv2.waitKey(2) & 0xFF == ord('q'):
-            break
+    with open('output.txt', 'w') as f:
+        while True:
+            # Read the next frame
+            ret, frame = cap.read()
 
-    cap.release()
-    cv2.destroyAllWindows()
+            frame = cv2.flip(frame, 1)
 
-fearValues = np.array(fearValues)
-avgFear = np.array(avgFear)
-x = np.arange(avgFear.size)
+            small_frame = cv2.resize(frame, (0, 0), fx=0.2, fy=0.2, interpolation = cv2.INTER_CUBIC)
 
-plt.plot(x, avgFear)
-plt.xlabel('x')
-plt.ylabel('fear')
-plt.show()
+            result = DeepFace.analyze(frame, actions = ['emotion'], enforce_detection=False)
+            print(result[0]['emotion']['fear'])
+            fearValues.append(result[0]['emotion']['fear'])
+            avgFearTemp = np.mean(fearValues[-60:])
+            avgFear.append(avgFearTemp)
+
+            f.write(str(avgFearTemp)+ '\n')
+            f.flush()
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = faceCascade.detectMultiScale(gray,1.1,4)
+
+            for (x, y, w, h) in faces:
+                cv2.rectangle(small_frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+            font = cv2.FONT_HERSHEY_SIMPLEX
+
+            cv2.putText(small_frame,
+                        str(result[0]['dominant_emotion']),
+                        (50, 200),
+                        font, 3,
+                        (0, 0, 255),
+                        2,
+                        cv2.LINE_4)
+
+            cv2.imshow('Video', small_frame)
+
+            if cv2.waitKey(2) & 0xFF == ord('q'):
+                break
+
+        cap.release()
+        cv2.destroyAllWindows()
+
+    fearValues = np.array(fearValues)
+    avgFear = np.array(avgFear)
+    x = np.arange(avgFear.size)
+
+    plt.plot(x, avgFear)
+    plt.xlabel('x')
+    plt.ylabel('fear')
+    plt.show()
+root = tk.Tk()
+root.protocol("WM_DELETE_WINDOW", on_closing)
