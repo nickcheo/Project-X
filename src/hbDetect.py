3f9cd1c6f2cbb63983b96d5a48dea23d090ae421
msrikar36 <smaduposu@gmail.com>
2023-03-05 04:08:11 -0500

    new

--- src/hbDetect.py
+++ src/hbDetect.py
@@ -1,40 +1,62 @@
 import cv2
 from deepface import DeepFace
+import numpy as np
+import matplotlib.pyplot as plt
+
+fearValues = []
+avgFear = []
 
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 cap = cv2.VideoCapture(0)
 
-while True:
-    # Read the next frame
-    ret, frame = cap.read()
+with open('output.txt', 'w') as f:
+    while True:
+        # Read the next frame
+        ret, frame = cap.read()
 
-    frame = cv2.flip(frame, 1)
+        frame = cv2.flip(frame, 1)
 
-    small_frame = cv2.resize(frame, (0, 0), fx=0.2, fy=0.2, interpolation = cv2.INTER_CUBIC)
+        small_frame = cv2.resize(frame, (0, 0), fx=0.2, fy=0.2, interpolation = cv2.INTER_CUBIC)
 
-    result = DeepFace.analyze(frame, actions = ['emotion'], enforce_detection=False)
-    print(result[0]['emotion'])
+        result = DeepFace.analyze(frame, actions = ['emotion'], enforce_detection=False)
+        print(result[0]['emotion']['fear'])
+        fearValues.append(result[0]['emotion']['fear'])
+        avgFearTemp = np.mean(fearValues[-60:])
+        avgFear.append(avgFearTemp)
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(gray,1.1,4)
+        f.write(str(avgFearTemp)+ '\n')
+        f.flush()
 
-    for (x, y, w, h) in faces:
-        cv2.rectangle(small_frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(gray,1.1,4)
 
-    font = cv2.FONT_HERSHEY_SIMPLEX
+        for (x, y, w, h) in faces:
+            cv2.rectangle(small_frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    cv2.putText(small_frame,
-                str(result[0]['dominant_emotion']),
-                (50, 200),
-                font, 3,
-                (0, 0, 255),
-                2,
-                cv2.LINE_4)
-    cv2.imshow('Video', small_frame)
+        font = cv2.FONT_HERSHEY_SIMPLEX
 
-    if cv2.waitKey(2) & 0xFF == ord('q'):
-        break
+        cv2.putText(small_frame,
+                    str(result[0]['dominant_emotion']),
+                    (50, 200),
+                    font, 3,
+                    (0, 0, 255),
+                    2,
+                    cv2.LINE_4)
 
-cap.release()
-cv2.destroyAllWindows()
+        cv2.imshow('Video', small_frame)
+
+        if cv2.waitKey(2) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+fearValues = np.array(fearValues)
+avgFear = np.array(avgFear)
+x = np.arange(avgFear.size)
+
+plt.plot(x, avgFear)
+plt.xlabel('x')
+plt.ylabel('fear')
+plt.show()
---
+++ src/sampleInput.py
@@ -0,0 +1,19 @@
+import time
+import numpy as np
+
+# l = ['1.2', '4.5']
+# n = np.array(float[l])
+
+print(n)
+
+# while True:
+#     with open('output.txt', 'r') as input_file:
+#         # read the last 10 lines of the file
+#         lines = input_file.readlines()[-10:]
+
+#         # process the lines
+#         for line in lines:
+#             process_data(line)
+
+#     # wait for some time before reading the file again
+#     time.sleep(1)
