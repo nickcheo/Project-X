721ccea7e3ef613f4ba1a1efc5a94b0dc3b5452a
nickcheo <99509288+nickcheo@users.noreply.github.com>
2023-03-07 20:18:21 -0500

    done with website for now

--- project-x/src/App.js
+++ project-x/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { BrowserRouter as Router, Route, Routes, NavLink } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import Home from './pages/Home';
@@ -7,27 +7,39 @@ import Questions from './pages/Questions';
 
 function App() {
   const [backgroundColor, setBackgroundColor] = useState('#888888');
+  const [textColor, setTextColor] = useState('black');
 
   const handleBackgroundColorChange = (color) => {
     setBackgroundColor(color);
   }
+
+  useEffect(() => {
+    const lightness = parseInt(backgroundColor.slice(11, -2));
+    if (lightness > 50) {
+      console.log("dark");
+      setTextColor('black');
+    } else {
+      setTextColor('white');
+      console.log(lightness);
+    }
+  }, [backgroundColor]);
+
   return (
     <div className="App">
       <Router>
-        <Navbar style={{ backgroundColor }} expand="lg">
+        <Navbar style={{ backgroundColor, color: textColor }} expand="lg">
           <Container>
             <NavLink to="/" exact className = "nav-link">
-             <Navbar.Brand href="#home"><NavLink to="/" style={{ color: 'black', textDecoration: 'none', }} >Productivity</NavLink></Navbar.Brand>
+             <Navbar.Brand href="#home"><NavLink to="/" style={{ color: textColor, textDecoration: 'none', fontWeight: 'bold', fontSize: '1.6rem', marginRight: '1rem', }} >Productivity</NavLink></Navbar.Brand>
             </NavLink>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
                 <NavLink to="/" exact className="nav-link">
-                Home
+                  <span style={{ color: textColor, textDecoration: 'none', fontSize: '1.1rem', marginRight: '1rem', }}>Home</span>
                 </NavLink>
-                <br></br>
                 <NavLink to="/about" exact className="nav-link">
-                About
+                  <span style={{ color: textColor, textDecoration: 'none', fontSize: '1.1rem', marginRight: '1rem', }}>About</span>
               </NavLink>
               </Nav>
             </Navbar.Collapse>
--- project-x/src/pages/Questions.js
+++ project-x/src/pages/Questions.js
@@ -1,38 +1,101 @@
 import React, { useState, useEffect } from 'react';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 function Questions({ backgroundColor }) {
-    const [value, setValue] = useState(50);
+  const [value, setValue] = useState(50);
+  const [questionIndex, setQuestionIndex] = useState(0);
 
-    const handleChange = (event) => {
-        setValue(event.target.value);
-    }
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
 
-    const rangeStyle = {
-        display: 'block',
-        margin: '0 auto',
-        width: '50%'
-    };
-
-    useEffect(() => {
-        updateBackgroundColor(value, backgroundColor);
-    }, [value, backgroundColor]);
-
-    function updateBackgroundColor(value, backgroundColor) {
-        const lightness = value;
-        const color = `hsl(0, 0%, ${lightness}%)`;
-        backgroundColor && backgroundColor(color);
-        document.body.style.background = color;
-        
-    }
+  const rangeStyle = {
+    display: 'block',
+    width: '50%',
     
+  };
 
-    return (
-        <>
-            <Form.Label>Range: {value}</Form.Label>
-            <Form.Range value={value} onChange={handleChange} style={rangeStyle} />
-        </>
-    );
+  useEffect(() => {
+    updateBackgroundColor(value, backgroundColor);
+  }, [value, backgroundColor]);
+
+  function updateBackgroundColor(value, backgroundColor) {
+    const lightness = value;
+    const color = `hsl(0, 0%, ${lightness}%)`;
+    backgroundColor && backgroundColor(color);
+    document.body.style.background = color;
+
+    // set text color to white if background is dark, otherwise set it to black
+    document.body.style.color = lightness <= 50 ? 'white' : 'black';
+  }
+
+  function handleNext() {
+    setQuestionIndex(questionIndex + 1);
+  }
+
+  function handleBack() {
+    setQuestionIndex(questionIndex - 1);
+  }
+
+  const questions = [
+    {
+      component: BrightnessQuestion,
+      props: { value, handleChange, rangeStyle },
+    },
+    {
+      component: MusicQuestion,
+      props: { backgroundColor, questionIndex },
+    },
+  ];
+
+  const CurrentQuestion = questions[questionIndex].component;
+  const currentQuestionProps = questions[questionIndex].props;
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '50vh',
+      }}
+    >
+      <CurrentQuestion {...currentQuestionProps} />
+      {questionIndex > 0 && (
+        <Button variant="primary" onClick={handleBack} style={{ marginRight: '1rem' }}>
+          Back
+        </Button>
+      )}
+      {questionIndex < questions.length - 1 ? (
+        <Button variant="primary" onClick={handleNext}>
+          Next
+        </Button>
+      ) : (
+        <Button variant="success">Finish</Button>
+      )}
+    </div>
+  );
+}
+
+function BrightnessQuestion({ value, handleChange, rangeStyle }) {
+  return (
+    <>
+      <h1 style={{ marginBottom: '2rem' }}>How bright would you like your lamp to be? </h1>
+      <Form.Range value={value} onChange={handleChange} style={rangeStyle} />
+      <h4>Brightness: {value}%</h4>
+    </>
+  );
+}
+
+function MusicQuestion({ backgroundColor, questionIndex }) {
+    console.log(questionIndex);
+  return (
+    <div style={{ backgroundColor: backgroundColor }}>
+      <h1 style={{ marginBottom: '2rem' }}>What kind of music do you like?</h1>
+    </div>
+  );
 }
 
 export default Questions;
